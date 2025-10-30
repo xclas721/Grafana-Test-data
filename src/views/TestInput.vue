bf5a46a760cf59d5242b58b82265b7d47698417b
HITRUST\kysonwang <kysonwang@hitrust.com>
2025-10-30 13:55:21 +0800

    移植vue第一版

---
+++ src/views/TestInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="height: 100vh; width: 100%; display: flex;">
+    <iframe
+      src="/Grafana-Test-Input.html"
+      style="border: 0; flex: 1; height: 100%; width: 100%;"
+      title="ACS 測試數據插入工具"
+    />
+  </div>
+</template>
+
+<style scoped></style>
+
+
